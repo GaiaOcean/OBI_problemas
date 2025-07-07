3a6a047fcd2b03255e04184754e60d156fbf2b92
Grazielle Almeida <86306468+GaiaOcean@users.noreply.github.com>
2025-07-07 09:53:07 -0300

    Add files via upload

--- fila_OBI.py
+++ fila_OBI.py
@@ -1,20 +1,13 @@
-#inverter o array
-#edge case se o primeiro or maior que todos, retornar quantidade de alunos -1
-
 
 def main() -> None:
 	qtd_alunos = int(input())
-	alunos_array = list()
 	reversed_array = list()
 	colas = 0
-	last_index = 0
 
-	for i in range(0,qtd_alunos -1): #filling up the entire array
-		altura = int(input())
-		alunos_array.append(altura)
+	alunos_array = list(map(int, input().split()))  # read all heights in one line
 	
-	for i in range(0,-1,-1): #goes from -1 all the way to 0 because the array is in reverse order
-		reversed_array.append(alunos_array[i]
+	for i in range(qtd_alunos-1,-1,-1): #goes from -1 all the way to 0 because the array is in reverse order
+		reversed_array.append(alunos_array[i])
 	
 	i = 1
 	j = 0
@@ -23,8 +16,11 @@ def main() -> None:
 		if reversed_array[i] <= reversed_array[j]:	
 			colas += 1
 		else:
-		   j = i
+			j = i     
+		
 		i += 1
+
+		
 	print(colas)
 
 if __name__ == "__main__":
